e35b470ee1552f023118870e25232984d1fbefe1
Sergey Bolotnikov <sbolotnikov@gmail.com>
2022-06-06 13:46:08 -0400

    framer-motion for pages added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,29 @@
 import '../styles/globals.css'
-import '../styles/navStyle.css';
-import '../styles/frontStyle.css';
-import type { AppProps } from 'next/app';
-import Layout from '../components/layout';
-import ThemeProvider from '../themeContext';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return ( 
-   <ThemeProvider>
-   <Layout>
-    <Component {...pageProps} />
-   </Layout>
-   </ThemeProvider>
+import '../styles/navStyle.css'
+import '../styles/frontStyle.css'
+import type { AppProps } from 'next/app'
+import Layout from '../components/layout'
+import ThemeProvider from '../themeContext'
+import { motion } from 'framer-motion'
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <ThemeProvider>
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: { opacity: 0, x: -200, y: 0 },
+          pageAnimate: { opacity: 1, x: 0, y: 0 },
+        }}
+        exit={{ opacity: 0, x: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </motion.div>
+    </ThemeProvider>
   )
 }
 
