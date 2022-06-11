3a5c439589599049c4f19c251116e3571c88b0d9
Sergey Bolotnikov <sbolotnikov@gmail.com>
2022-06-10 21:02:37 -0400

    add Alert support contact form start

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,46 +4,69 @@ import Link from 'next/link'
 import { useRouter } from 'next/router';
 import { useState, useRef } from 'react';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import AlertMenu from "../components/alertMenu";
 function contact() {
   const form = useRef();
   const { t } = useTranslation()
   let router = useRouter();
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [message, setMessage]= useState("");
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>)=>{
-    event.preventDefault();
-    emailjs.sendForm(process.env.NEXT_PUBLIC_SERVICE_ID!, process.env.NEXT_PUBLIC_TEMPLATE_ID!, event.target as HTMLFormElement,process.env.NEXT_PUBLIC_PUBLIC_KEY)
-    .then((result: EmailJSResponseStatus) => {
-      console.log(result.text);
-    }, (error) => {
-      console.log(error.text);
-    });
-
-
-      // let data = {
-      //     name,
-      //     email,
-      //     message,
-      //     mainEmail:'serge.bolotnikov@gmail.com'
-      //   }
-      //   console.log( data)
-      // fetch('/api/sendemail', {
-      //     method: 'POST',
-      //     headers: {
-      //       'Accept': 'application/json, text/plain, */*',
-      //       'Content-Type': 'application/json'
-      //     },
-      //     body: JSON.stringify(data)
-      //   }).then((res) => {
-      //       console.log(res)
-      //     if (res.status === 200) {   
-      //       setName('')
-      //       setEmail('')
-      //       setMessage('')
-      //     }
-      //   })
+  const [revealAlert, setRevealAlert] = useState(false);
+  const [alertStyle, setAlertStyle] = useState({
+    variantHead: '',
+    heading: '',
+    text: ``,
+    color1: '',
+    button1: '',
+    color2: '',
+    button2: '',
+    inputField:""
+  });
+  const onReturn =  (decision1:string, inputValue:string | null) => {
+    setRevealAlert(false);
+    console.log(decision1, inputValue);
+  }
+  const handleSubmit = 
+  // (event: React.FormEvent<HTMLFormElement>)=>{
+  //   event.preventDefault();
+  //   let form=event.target.value
+  //   console.log(form["user_email"])
+    (event: React.SyntheticEvent) => {
+      event.preventDefault();
+      const target1 = event.target as typeof event.target & {
+        user_name: { value: string };
+        user_email: { value: string };
+        message:{value: string };
+      };
+      const name = target1.user_name.value; // typechecks!
+      const email = target1.user_email.value;
+      const message = target1.message.value;
+      console.log(name, email, message)
+    // emailjs.sendForm(process.env.NEXT_PUBLIC_SERVICE_ID!, process.env.NEXT_PUBLIC_TEMPLATE_ID!, event.target as HTMLFormElement,process.env.NEXT_PUBLIC_PUBLIC_KEY)
+    // .then((result: EmailJSResponseStatus) => {
+    //   setAlertStyle({
+    //     variantHead: 'info',
+    //     heading: t("common:Message"),
+    //     text: `${t("common:Email success")} ${result.text}`,
+    //     color1: 'success',
+    //     button1: t("common:Return"),
+    //     color2: '',
+    //     button2: '',
+    //     inputField:""
+    //   });
+    //   setRevealAlert(true); 
+    // }, (error) => {
+    //   console.log(error.text);
+    //   setAlertStyle({
+    //     variantHead: 'danger',
+    //     heading: t('common:Warning'),
+    //     text: `${t("common:Email fail")} ${error.text}`,
+    //     color1: 'warning',
+    //     button1: t("common:Return"),
+    //     color2: '',
+    //     button2: '',
+    //     inputField:""
+    //   });
+    //   setRevealAlert(true); 
+    // });
   }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2  dark:text-light">
@@ -52,10 +75,11 @@ function contact() {
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <main >
+    {revealAlert && <AlertMenu onReturn={onReturn} styling={alertStyle} />}
         <div className="absolute top-0 left-0 h-[100vh] w-[100vw] flex justify-center  items-center">
           <form
             onSubmit={handleSubmit}
-            className="w-[80%] h-[80%] max-w-[700px] max-h-[700px] rounded-md flex flex-col justify-between  items-center p-2"
+            className="w-[80%] h-[80%] max-w-[700px] max-h-[700px] rounded-md flex flex-col justify-between  items-center p-2 "
             style={{ boxShadow: '0 0 150px rgb(244 217 190 / 85%)'}}
           > 
             <h1 className=" font-bold text-center text-5xl">{t("contact:haveaQuestion")}</h1>
@@ -66,29 +90,27 @@ function contact() {
 
             <input
               name="user_name"
-              className="w-full rounded "
+              className="w-full rounded text-lightteal"
               type="text"
               placeholder={t("common:yourName")}
-              required
-              // onChange={(e)=>{setName(e.target.value)}}
-              // value = {name}
-              minLength={3}
+             
+              // minLength={3}
             />
 
             <input
               name="user_email" 
-              className="w-full rounded "
-              type="email" 
+              className="w-full rounded text-lightteal"
+              type="text" 
               placeholder={t("common:email")}
-              required
+              
               // onChange={(e)=>{setEmail(e.target.value)}}
               // value = {email}
             />
             <textarea
               name="message"
-              className="w-full rounded "
+              className="w-full rounded text-lightteal"
               placeholder={t("contact:anyquestions")}
-              required
+              
               // onChange={(e)=>{setMessage(e.target.value)}}
               // value = {message}
               minLength={5}
@@ -99,6 +121,22 @@ function contact() {
               {t("contact:sendMessage")}
             </button>
           </form>
+          {/* <button  className="rounded-full bg-lightteal text-lightlavender p-2 transition delay-150 m-2 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-lightblue"
+           onClick={() => {
+            setAlertStyle({
+              
+              variantHead: 'danger',
+              heading: t("common:Message"),
+              text: `${t("common:Email success")} `,
+              color1: 'success',
+              button1: t("common:Return"),
+              color2: '',
+              button2: '',
+              inputField:""
+            });
+            setRevealAlert(true);    
+           }}
+          >test</button> */}
         </div>
       
 
